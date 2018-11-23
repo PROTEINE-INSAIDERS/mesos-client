45d7a66f2ce3af1714be344a26c764b760cadd04
Черничкин Станислав Викторович <Chernichkin.S.Vi@sberbank.ru>
2018-11-23 16:25:26 +0300

    Test app to explore  endpoint

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,22 @@
-module Main where
+{-# LANGUAGE OverloadedStrings #-}
 
-import Lib
+import           Network.HTTP.Client
+
+-- TODO:
+-- 1. Write scripts to download .proto and generate files
+-- e.g. wget ...
+--      hprotoc -d src -Iproto -p  Mesos.V1 -l proto/mesos/v1/mesos.proto proto/mesos/v1/scheduler/scheduler.proto
+-- 2. Add operator API
 
 main :: IO ()
-main = someFunc
+main = do
+    let settings = managerSetProxy
+            (proxyEnvironment Nothing)
+            defaultManagerSettings
+    man <- newManager settings
+    let req = "http://localhost:5050/api/v1"
+            { method = "POST"
+            , requestBody = RequestBodyLBS "{ \"type\": \"GET_HEALTH\" }"
+            , requestHeaders = [ ("Content-Type", "application/json") ]
+            }
+    httpLbs req man >>= print
