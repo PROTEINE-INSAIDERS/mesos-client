34954aa4c8fcf040562d734fa7f038ba6b4e8a60
Stanislav Chernichkin <schernichkin@gmail.com>
2018-11-28 22:15:03 +0300

    int

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,27 @@
 {-# LANGUAGE OverloadedStrings #-}
 
+import           Data.Either
+import           Mesos.V1.Master.Protos.Call
+import           Mesos.V1.Master.Protos.Call.Type
+import qualified Mesos.V1.Master.Protos.Response  as Master
 import           Network.HTTP.Client
+import           Text.ProtocolBuffers
+import           Text.ProtocolBuffers.Basic
 
 -- TODO:
 -- Разделить интерфейс на два вида ответов
 -- 200 - обработано немедленно.
 -- 202 - поточная обработка.
+
+getHealth :: Call
+getHealth = defaultValue { type' = Just GET_HEALTH }
+
+getHealthLBS = messagePut getHealth
+
+parseResponse :: ByteString -> Master.Response
+parseResponse bs = case messageGet bs of
+                     Left e         -> error e
+                     Right (msg, _) -> msg
 
 main :: IO ()
 main = do
@@ -15,9 +31,14 @@ main = do
     man <- newManager settings
     let req = "http://localhost:5050/api/v1"
             { method = "POST"
-            , requestBody = RequestBodyLBS "{ \"type\": \"GET_HEALTH\" }"
-            , requestHeaders = [ ("Content-Type", "application/json")
+            , requestBody = RequestBodyLBS getHealthLBS
+            , requestHeaders = [ ("Content-Type", "application/x-protobuf")
                                , ("Accept", "application/x-protobuf")
                                ]
             }
-    httpLbs req man >>= print
+    res <- httpLbs req man
+    print "Response: "
+    print res
+    print "Proto: "
+    print $ parseResponse  $ responseBody res
+    return ()
